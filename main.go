ceb040261de0a56eb9ca838042849f461f8f0503
Ilya Mudritskiy <ilya.mudritskiy@gmail.com>
2023-05-26 14:54:09 +0100

    Minor changes

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"github.com/ilyamudritskiy/go-poker/objects"
+)
+
+func main() {
+	var deck = objects.GetDeck()
+	objects.PrintTable()
+	var hand = deck.DealHand(0)
+	hand.Print("big")
+	
+}
