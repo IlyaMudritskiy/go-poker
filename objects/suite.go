a66afae0c084739742264504b9192f75bd64b561
Ilya Mudritskiy <ilya.mudritskiy@gmail.com>
2023-05-26 14:53:49 +0100

    Added suites and values

---
+++ objects/suite.go
@@ -0,0 +1,13 @@
+package objects
+
+// Unicode symbols for suites
+var Spades string = "\u2660"
+var Hearts string = "\u2665"
+var Diamonds string = "\u2666"
+var Clubs string = "\u2663"
+
+// Slice with all suites
+var suites = []string{Spades, Hearts, Diamonds, Clubs}
+
+// Slice with all card values
+var cardValues = []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
