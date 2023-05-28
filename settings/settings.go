984590e390cbbfbd3d9eee92df9dc18a879b51c5
Ilya Mudritskiy <ilya.mudritskiy@gmail.com>
2023-05-28 16:09:09 +0100

    Added settings to the program

---
+++ settings/settings.go
@@ -0,0 +1,14 @@
+package settings
+
+import (
+	"github.com/ilyamudritskiy/go-poker/utils"
+)
+
+var BackColor = utils.Yellow
+
+var HeartsColor = utils.Red
+var DiamindsColor = utils.Red
+var ClubsColor = utils.Blue
+var SpadesColor = utils.Blue
+
+var UsedColor = utils.Red
